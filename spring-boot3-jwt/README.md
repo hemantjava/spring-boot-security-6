2da50928c2e5fd4aaefbd411b740a50fdcec248d
Hemant Kumar Sahu <hemantjava90@gmail.com>
2024-09-04 06:15:33 +0530

    Add files via upload

---
+++ spring-boot3-jwt/README.md
@@ -0,0 +1,17 @@
+# spring-boot3-jwt
+
+```http://localhost:8080/products/new
+{
+"name":"hemant",
+"email":"hemant@gmail.com",
+"password":"password",
+"roles":"ROLE_ADMIN"
+}
+
+http://localhost:8080/products/authenticate
+{
+"username":"hemant",
+
+"password":"password"
+}```
+
---
+++ spring-boot3-jwt/src/main/java/com/hemant/controller/ProductController.java
@@ -0,0 +1,63 @@
+package com.hemant.controller;
+
+import com.hemant.dto.AuthRequest;
+import com.hemant.dto.Product;
+import com.hemant.entity.UserInfo;
+import com.hemant.service.JwtService;
+import com.hemant.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+
+    private final ProductService service;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+    @PostMapping("/new")
+    public String addNewUser(@RequestBody UserInfo userInfo) {
+        return service.addUser(userInfo);
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public List<Product> getAllTheProducts() {
+        return service.getProducts();
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public Product getProductById(@PathVariable int id) {
+        return service.getProduct(id);
+    }
+
+
+    @PostMapping("/authenticate")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
+
+
+    }
+}
