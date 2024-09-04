1602caa0dd476614f5f80a2efcb790da3f693ed5
Hemant Kumar sahu <Hemantjava90@gmail.com>
2024-09-04 06:28:51 +0530

    update the classes

---
+++ spring-boot-3-jwt-security/src/main/java/com/hemant/security/book/BookRepository.java
@@ -0,0 +1,6 @@
+package com.hemant.security.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
---
+++ spring-boot-3-jwt-security/src/main/java/com/hemant/security/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.hemant.security.token;
+
+public enum TokenType {
+  BEARER
+}
